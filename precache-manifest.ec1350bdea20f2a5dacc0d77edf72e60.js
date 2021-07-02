fc450080d4ffaac6d91d286adccd8aae12061bd5
Prashant Jogi <prashant.jogi@cdk.com>
2021-07-02 19:04:46 +0530

    Updates

---
+++ precache-manifest.ec1350bdea20f2a5dacc0d77edf72e60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6186f80e6db2b330692c1ee16040d01",
+    "url": "/demo-app/index.html"
+  },
+  {
+    "revision": "9f57cab9126152e3d6d6",
+    "url": "/demo-app/static/css/main.19f271d9.chunk.css"
+  },
+  {
+    "revision": "48be77e965f30e9929b6",
+    "url": "/demo-app/static/js/2.80ddeb5c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/demo-app/static/js/2.80ddeb5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f57cab9126152e3d6d6",
+    "url": "/demo-app/static/js/main.d30714c0.chunk.js"
+  },
+  {
+    "revision": "9593045f8ea9008eaf45",
+    "url": "/demo-app/static/js/runtime-main.fac6da4c.js"
+  },
+  {
+    "revision": "287dc17e8799fb9667272f4820c8d72e",
+    "url": "/demo-app/static/media/style.287dc17e.less"
+  }
+]);
